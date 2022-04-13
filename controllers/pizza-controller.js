0baebc455ce23b5344c1631ba53023b3ac22c1c5
Marcus Morua <91860506+mmorua@users.noreply.github.com>
2022-04-12 22:19:06 -0500

    feat: User can CRUD data for a pizza's comments in the database

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -4,22 +4,42 @@ const pizzaController = {
   // get all pizzas
   getAllPizza(req, res) {
     Pizza.find({})
-      .then(dbPizzaData => res.json(dbPizzaData))
-      .catch(err => {
-        console.log(err);
-        res.sendStatus(400);
-      });
+    .populate({
+      path: 'comments',
+      // select will remove __v field from the comments hence the path within this method
+      select: '-__v'
+    })
+    // select will remove __v field from the pizza
+    .select('-__v')
+    // sort will return the newest pizza in descending order by the _id value
+    .sort({_id: -1})
+    .then(dbPizzaData => res.json(dbPizzaData))
+    .catch(err => {
+      console.log(err);
+      res.sendStatus(400);
+    });
   },
 
   // get one pizza by id
   getPizzaById({ params }, res) {
     Pizza.findOne({ _id: params.id })
-      .then(dbPizzaData => res.json(dbPizzaData))
-      .catch(err => {
-        console.log(err);
-        res.sendStatus(400);
-      });
-  },
+    .populate({
+      path: 'comments',
+      select: '-__v'
+    })
+    .select('-__v')
+    .then(dbPizzaData => {
+      if (!dbPizzaData) {
+        res.status(404).json({ message: 'No pizza found with this id!' });
+        return;
+      }
+      res.json(dbPizzaData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+},
 
   // createPizza
   createPizza({ body }, res) {
--- models/index.js
+++ models/index.js
@@ -1,3 +1,6 @@
+// importing the Pizza and Comment models so it's accessible from one file
 const Pizza = require('./Pizza');
+const Comment = require('./Comment');
 
-module.exports = { Pizza };
+// exporting the Pizza and Comment models so it's accessible from one file
+module.exports = { Pizza, Comment };
